15a484aa218791c44ad16d193172ccc1bbd3282c
mithhu <mahamudul.hasan@evaly.com.bd>
2020-09-23 16:06:02 +0600

    google trend

---
+++ index.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const Sheet = require("./sheet");
+
+(async function () {
+  const res = await fetch(
+    "https://explodingtopics.com/featured-topics-this-month"
+  );
+  const text = await res.text();
+  const $ = cheerio.load(text);
+  const containers = $(".topicInfoContainer").toArray();
+  const trends = containers.map((c) => {
+    const active = $(c);
+    const keyword = active.find(".tileKeyword").text();
+    const description = active.find(".tileDescription").text();
+    const searchesPerMonth = active
+      .find(".scoreTag")
+      .first()
+      .text()
+      .split("mo")[1];
+    return { keyword, description, searchesPerMonth };
+  });
+
+  const sheet = new Sheet();
+  await sheet.load();
+  await sheet.addRows(trends);
+})();
